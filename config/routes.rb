1f1ddbf57b2f100b4ff12fcdde86eed141f9e621
Chester Syron <csyron.damian14@gmail.com>
2018-09-26 16:38:02 +0800

    Uploading and Show Image working fine, remove turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :images
   default_url_options :host => "http://localhost:3000"
   devise_for :users, :controllers => { :registrations => 'registrations' }
   root 'welcome#index'
